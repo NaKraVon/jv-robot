a49e142d00c0a0f1c31f0ff8b7a54699a004c778
mr-Edoc <mr.edoc.work@gmail.com>
2025-10-05 13:09:08 +0300

    Ver1/Developed first version of robot finding rout

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,64 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+
+        if (currentX < toX) {
+            ensureDirection(robot, Direction.RIGHT);
+        } else if (currentX > toX) {
+            ensureDirection(robot, Direction.LEFT);
+        }
+
+        while (robot.getX() != toX) {
+            robot.stepForward();
+        }
+
+        if (currentY < toY) {
+            ensureDirection(robot, Direction.UP);
+        } else if (currentY > toY) {
+            ensureDirection(robot, Direction.DOWN);
+        }
+
+        while (robot.getY() != toY) {
+            robot.stepForward();
+        }
+    }
+
+    private void ensureDirection(Robot robot, Direction desiredDirection) {
+        if (robot.getDirection() == desiredDirection) {
+            return;
+        }
+
+        Direction current = robot.getDirection();
+        Direction leftTurnDirection = getDirectionAfterTurn(current, true);
+        Direction rightTurnDirection = getDirectionAfterTurn(current, false);
+
+        if (leftTurnDirection == desiredDirection) {
+            robot.turnLeft();
+        } else if (rightTurnDirection == desiredDirection) {
+            robot.turnRight();
+        } else {
+            robot.turnLeft();
+            robot.turnLeft();
+        }
+    }
+
+    private Direction getDirectionAfterTurn(Direction current, boolean turnLeft) {
+        if (turnLeft) {
+            return switch (current) {
+                case UP -> Direction.LEFT;
+                case LEFT -> Direction.DOWN;
+                case DOWN -> Direction.RIGHT;
+                case RIGHT -> Direction.UP;
+            };
+        } else {
+            return switch (current) {
+                case UP -> Direction.RIGHT;
+                case RIGHT -> Direction.DOWN;
+                case DOWN -> Direction.LEFT;
+                case LEFT -> Direction.UP;
+            };
+        }
     }
 }
